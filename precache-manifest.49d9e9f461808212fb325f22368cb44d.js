23b5fe71dfafd944e262f3e1de51525ee3f25d3d
Sandesh Menjoge <smenjoge@gmail.com>
2020-07-26 11:20:30 -0400

    Updates

---
+++ precache-manifest.49d9e9f461808212fb325f22368cb44d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280b71cb41054d8ed7743ed6af756ef3",
+    "url": "/newfobjs/index.html"
+  },
+  {
+    "revision": "ba2fbba6007ff301ed4a",
+    "url": "/newfobjs/static/css/main.77ac8097.chunk.css"
+  },
+  {
+    "revision": "36e035feef019b3f0baa",
+    "url": "/newfobjs/static/js/2.d96b91c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newfobjs/static/js/2.d96b91c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2fbba6007ff301ed4a",
+    "url": "/newfobjs/static/js/main.87b4af1a.chunk.js"
+  },
+  {
+    "revision": "463d0657e8686cddf10d",
+    "url": "/newfobjs/static/js/runtime-main.dce6c04b.js"
+  }
+]);
